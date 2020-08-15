0e31cbe443fbd6f8c0bb0fdf5700adb01311548d
Anej Svete <anej.svete@protonmail.com>
2020-08-15 14:35:15 +0200

    Import correction.

--- spinup/utils/algorithm_logging.py
+++ spinup/utils/algorithm_logging.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from utils.constants import PlottingConstants
+from spinup.utils.constants import PlottingConstants
 
 
 class AlgorithmLogger:
